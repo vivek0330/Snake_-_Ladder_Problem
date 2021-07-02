6efffddd6daf3d68926df7d7e8707fdd58e15e4d
Vivek varshney <Vivek.iitd93@gmail.com>
2021-07-02 23:32:29 +0530

    the number of times the dice was played to win the game

--- SnakeLadder/src/SnakeAndLadder/SnakeAndLadderGame.java
+++ SnakeLadder/src/SnakeAndLadder/SnakeAndLadderGame.java
@@ -6,36 +6,40 @@ public class SnakeAndLadderGame {
 	static int POSITION_OF_PLAYER = 0;
 	static int START_POSITION = 0;
 	static int END_POSITION = 100;
+	static int COUNT = 0;
 
 	public static void main(String[] args) {
-		
+
 		// Play the game till the end position
 		while (POSITION_OF_PLAYER != END_POSITION) {
-			
+
 			// Generate random value
 			int RANDOM_DIE_VALUE = (int) (Math.random() * 6) + 1;
 			int RANDOM_CHECK = (int) (Math.random() * 10) % 3;
-			
+
 			// Check the condition
 			switch (RANDOM_CHECK) {
 			case 1:
+				COUNT++;
 				System.out.println("Ladder");
 				System.out.println("Dice value: " + RANDOM_DIE_VALUE);
 				POSITION_OF_PLAYER += RANDOM_DIE_VALUE;
 				System.out.println("Player position is: " + POSITION_OF_PLAYER);
 				if (POSITION_OF_PLAYER > END_POSITION) {
 					POSITION_OF_PLAYER -= RANDOM_DIE_VALUE;
-				}
-				else if (POSITION_OF_PLAYER == END_POSITION) {
+				} else if (POSITION_OF_PLAYER == END_POSITION) {
+					System.out.println("Number of times the dice was played: " + COUNT);
 					System.out.println("Player position is: " + POSITION_OF_PLAYER);
 					System.out.println("Player Won !!!");
 					break;
 				}
+				System.out.println("Number of times the dice was played: " + COUNT);
 				System.out.println("player Position: " + POSITION_OF_PLAYER);
 				System.out.println("");
 				break;
 
 			case 2:
+				COUNT++;
 				System.out.println("Snake");
 				System.out.println("Dice value: " + RANDOM_DIE_VALUE);
 				POSITION_OF_PLAYER -= RANDOM_DIE_VALUE;
@@ -43,6 +47,7 @@ public class SnakeAndLadderGame {
 					POSITION_OF_PLAYER = START_POSITION;
 					System.out.println("player Position: " + POSITION_OF_PLAYER);
 				}
+				System.out.println("Number of times the dice was played: " + COUNT);
 				System.out.println("player Position: " + POSITION_OF_PLAYER);
 				System.out.println("");
 				break;
@@ -57,6 +62,10 @@ public class SnakeAndLadderGame {
 			default:
 				System.out.println("Something Unexpectated Happen !!");
 
+			}
+			// condition check for negative position
+			if (POSITION_OF_PLAYER < START_POSITION) {
+				POSITION_OF_PLAYER = START_POSITION;
 			}
 
 		}
