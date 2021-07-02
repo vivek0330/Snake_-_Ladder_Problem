e4e7fd7827364d189c8aacee2c90129a8314aa8a
Vivek varshney <Vivek.iitd93@gmail.com>
2021-07-02 22:38:14 +0530

    Ensure the player gets to exact winning position 100

--- SnakeLadder/src/SnakeAndLadder/SnakeAndLadderGame.java
+++ SnakeLadder/src/SnakeAndLadder/SnakeAndLadderGame.java
@@ -8,11 +8,14 @@ public class SnakeAndLadderGame {
 	static int END_POSITION = 100;
 
 	public static void main(String[] args) {
+		
 		// Play the game till the end position
 		while (POSITION_OF_PLAYER != END_POSITION) {
+			
 			// Generate random value
 			int RANDOM_DIE_VALUE = (int) (Math.random() * 6) + 1;
 			int RANDOM_CHECK = (int) (Math.random() * 10) % 3;
+			
 			// Check the condition
 			switch (RANDOM_CHECK) {
 			case 1:
@@ -21,12 +24,13 @@ public class SnakeAndLadderGame {
 				POSITION_OF_PLAYER += RANDOM_DIE_VALUE;
 				System.out.println("Player position is: " + POSITION_OF_PLAYER);
 				if (POSITION_OF_PLAYER > END_POSITION) {
-					POSITION_OF_PLAYER = END_POSITION;
-					System.out.println("player Position: " + POSITION_OF_PLAYER);
+					POSITION_OF_PLAYER -= RANDOM_DIE_VALUE;
+				}
+				else if (POSITION_OF_PLAYER == END_POSITION) {
+					System.out.println("Player position is: " + POSITION_OF_PLAYER);
 					System.out.println("Player Won !!!");
 					break;
 				}
-
 				System.out.println("player Position: " + POSITION_OF_PLAYER);
 				System.out.println("");
 				break;
@@ -35,12 +39,10 @@ public class SnakeAndLadderGame {
 				System.out.println("Snake");
 				System.out.println("Dice value: " + RANDOM_DIE_VALUE);
 				POSITION_OF_PLAYER -= RANDOM_DIE_VALUE;
-				System.out.println("Player position is: " + POSITION_OF_PLAYER);
 				if (POSITION_OF_PLAYER < START_POSITION) {
 					POSITION_OF_PLAYER = START_POSITION;
 					System.out.println("player Position: " + POSITION_OF_PLAYER);
 				}
-
 				System.out.println("player Position: " + POSITION_OF_PLAYER);
 				System.out.println("");
 				break;
@@ -55,11 +57,6 @@ public class SnakeAndLadderGame {
 			default:
 				System.out.println("Something Unexpectated Happen !!");
 
-			}
-
-			// Condition check for player position
-			if (POSITION_OF_PLAYER < START_POSITION) {
-				POSITION_OF_PLAYER = START_POSITION;
 			}
 
 		}
