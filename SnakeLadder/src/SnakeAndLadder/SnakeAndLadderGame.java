485be3fde79c68f55aaeb83e6638ffb1fd394314
Vivek varshney <Vivek.iitd93@gmail.com>
2021-07-02 17:03:19 +0530

    Player rolls a die and get a number between 1 to 6

--- SnakeLadder/src/SnakeAndLadder/SnakeAndLadderGame.java
+++ SnakeLadder/src/SnakeAndLadder/SnakeAndLadderGame.java
@@ -4,10 +4,18 @@ public class SnakeAndLadderGame {
 
 	// Single player Start position is 0;
 	public static int POSITION_OF_PLAYER_1 = 0;
+	
+	//Player Rolls The Die and get a value between 1 to 6;
+    public static void dieRoll()
+    {
+            int die_value=(int)(Math.random()*6)+1;
+            System.out.println("DieValue: " + die_value);
+    }
 
 	public static void main(String[] args) {
 		// Welcome initialize message
 		System.out.println("Welcome to the Snake & Ladder Game");
-
+		SnakeAndLadderGame obj1=new SnakeAndLadderGame();
+        obj1.dieRoll();
 	}
 }
