c301d2aaee32b349374b0b59100b5c8d1820aef0
Vivek varshney <Vivek.iitd93@gmail.com>
2021-07-03 00:48:59 +0530

    which player won the game

--- SnakeLadder/src/SnakeAndLadder/SnakeAndLadderGame.java
+++ SnakeLadder/src/SnakeAndLadder/SnakeAndLadderGame.java
@@ -3,59 +3,82 @@ package SnakeAndLadder;
 public class SnakeAndLadderGame {
 
 	// Variables and constant initialized
-	static int POSITION_OF_PLAYER = 0;
+	static int POSITION_OF_PLAYER_1 = 0;
+	static int POSITION_OF_PLAYER_2 = 0;
 	static int START_POSITION = 0;
 	static int END_POSITION = 100;
-	static int COUNT = 0;
 
 	public static void main(String[] args) {
 
 		// Play the game till the end position
-		while (POSITION_OF_PLAYER != END_POSITION) {
+		while (POSITION_OF_PLAYER_1 != END_POSITION && POSITION_OF_PLAYER_2 != END_POSITION) {
 
 			// Generate random value
-			int RANDOM_DIE_VALUE = (int) (Math.random() * 6) + 1;
+			int RANDOM_DIE_VALUE_1 = (int) (Math.random() * 6) + 1;
+			int RANDOM_DIE_VALUE_2 = (int) (Math.random() * 6) + 1;
 			int RANDOM_CHECK = (int) (Math.random() * 10) % 3;
 
 			// Check the condition
 			switch (RANDOM_CHECK) {
 			case 1:
-				COUNT++;
 				System.out.println("Ladder");
-				System.out.println("Dice value: " + RANDOM_DIE_VALUE);
-				POSITION_OF_PLAYER += RANDOM_DIE_VALUE;
-				System.out.println("Player position is: " + POSITION_OF_PLAYER);
-				if (POSITION_OF_PLAYER > END_POSITION) {
-					POSITION_OF_PLAYER -= RANDOM_DIE_VALUE;
-				} else if (POSITION_OF_PLAYER == END_POSITION) {
-					System.out.println("Number of times the dice was played: " + COUNT);
-					System.out.println("Player position is: " + POSITION_OF_PLAYER);
-					System.out.println("Player Won !!!");
+				System.out.println("Dice 1 value: " + RANDOM_DIE_VALUE_1);
+				System.out.println("Dice 2 value: " + RANDOM_DIE_VALUE_2);
+				POSITION_OF_PLAYER_1 += RANDOM_DIE_VALUE_1;
+				POSITION_OF_PLAYER_2 += RANDOM_DIE_VALUE_2;
+				System.out.println("Player 1 position is: " + POSITION_OF_PLAYER_1);
+				System.out.println("Player 2 position is: " + POSITION_OF_PLAYER_2);
+
+				// player 1 condition
+				if (POSITION_OF_PLAYER_1 > END_POSITION) {
+					POSITION_OF_PLAYER_1 -= RANDOM_DIE_VALUE_1;
+				} else if (POSITION_OF_PLAYER_1 == END_POSITION) {
+					System.out.println("Player 1 position is: " + POSITION_OF_PLAYER_1);
+					System.out.println("Player 1 Won !!!");
 					break;
 				}
-				System.out.println("Number of times the dice was played: " + COUNT);
-				System.out.println("player Position: " + POSITION_OF_PLAYER);
+				System.out.println("player 1 Position: " + POSITION_OF_PLAYER_1);
+
+				// player 2 condition
+				if (POSITION_OF_PLAYER_2 > END_POSITION) {
+					POSITION_OF_PLAYER_2 -= RANDOM_DIE_VALUE_2;
+				} else if (POSITION_OF_PLAYER_2 == END_POSITION) {
+					System.out.println("Player 2 position is: " + POSITION_OF_PLAYER_2);
+					System.out.println("Player 2 Won !!!");
+					break;
+				}
+				System.out.println("player 2 Position: " + POSITION_OF_PLAYER_2);
 				System.out.println("");
 				break;
 
 			case 2:
-				COUNT++;
 				System.out.println("Snake");
-				System.out.println("Dice value: " + RANDOM_DIE_VALUE);
-				POSITION_OF_PLAYER -= RANDOM_DIE_VALUE;
-				if (POSITION_OF_PLAYER < START_POSITION) {
-					POSITION_OF_PLAYER = START_POSITION;
-					System.out.println("player Position: " + POSITION_OF_PLAYER);
+				System.out.println("Dice 1 value: " + RANDOM_DIE_VALUE_1);
+				System.out.println("Dice 2 value: " + RANDOM_DIE_VALUE_2);
+
+				POSITION_OF_PLAYER_1 -= RANDOM_DIE_VALUE_1;
+				POSITION_OF_PLAYER_2 -= RANDOM_DIE_VALUE_2;
+
+				if (POSITION_OF_PLAYER_1 < START_POSITION) {
+					POSITION_OF_PLAYER_1 = START_POSITION;
+					System.out.println("player 1 Position: " + POSITION_OF_PLAYER_1);
 				}
-				System.out.println("Number of times the dice was played: " + COUNT);
-				System.out.println("player Position: " + POSITION_OF_PLAYER);
+				System.out.println("player 1 Position: " + POSITION_OF_PLAYER_1);
+
+				if (POSITION_OF_PLAYER_2 < START_POSITION) {
+					POSITION_OF_PLAYER_2 = START_POSITION;
+					System.out.println("player 2 Position: " + POSITION_OF_PLAYER_2);
+				}
+				System.out.println("player 2 Position: " + POSITION_OF_PLAYER_2);
 				System.out.println("");
 				break;
 
 			case 0:
 				System.out.println("No Play");
-				POSITION_OF_PLAYER = POSITION_OF_PLAYER;
-				System.out.println("Player position is: " + POSITION_OF_PLAYER);
+				POSITION_OF_PLAYER_1 = POSITION_OF_PLAYER_1;
+				POSITION_OF_PLAYER_2 = POSITION_OF_PLAYER_2;
+				System.out.println("Player 1 position is: " + POSITION_OF_PLAYER_1);
+				System.out.println("Player 2 position is: " + POSITION_OF_PLAYER_2);
 				System.out.println("");
 				break;
 
@@ -63,9 +86,12 @@ public class SnakeAndLadderGame {
 				System.out.println("Something Unexpectated Happen !!");
 
 			}
-			// condition check for negative position
-			if (POSITION_OF_PLAYER < START_POSITION) {
-				POSITION_OF_PLAYER = START_POSITION;
+			// condition check for negative position of the player
+			if (POSITION_OF_PLAYER_1 < START_POSITION) {
+				POSITION_OF_PLAYER_1 = START_POSITION;
+			}
+			if (POSITION_OF_PLAYER_2 < START_POSITION) {
+				POSITION_OF_PLAYER_2 = START_POSITION;
 			}
 
 		}
