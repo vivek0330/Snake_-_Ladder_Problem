2cd01a28799e0d2d49e0a991378003d4775bec4f
Vivek varshney <Vivek.iitd93@gmail.com>
2021-07-02 16:53:45 +0530

    Declared single player at start position is 0

--- SnakeLadder/src/SnakeAndLadder/SnakeAndLadderGame.java
+++ SnakeLadder/src/SnakeAndLadder/SnakeAndLadderGame.java
@@ -2,7 +2,11 @@ package SnakeAndLadder;
 
 public class SnakeAndLadderGame {
 
+	// Single player Start position is 0;
+	public static int POSITION_OF_PLAYER_1 = 0;
+
 	public static void main(String[] args) {
+		// Welcome initialize message
 		System.out.println("Welcome to the Snake & Ladder Game");
 
 	}
