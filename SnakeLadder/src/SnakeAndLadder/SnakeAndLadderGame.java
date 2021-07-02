7c40b44b5415f86841493af2a20892e93c450e45
Vivek varshney <Vivek.iitd93@gmail.com>
2021-07-02 19:18:53 +0530

    the player then checks a option they are no Play, Snake, Ladder

--- SnakeLadder/src/SnakeAndLadder/SnakeAndLadderGame.java
+++ SnakeLadder/src/SnakeAndLadder/SnakeAndLadderGame.java
@@ -2,19 +2,38 @@ package SnakeAndLadder;
 
 public class SnakeAndLadderGame {
 
-	// Single player Start position is 0;
-	public static int POSITION_OF_PLAYER_1 = 0;
-
-	// Player Rolls The Die and get a value between 1 to 6;
-	public static void dieRoll() {
-		int die_value = (int) (Math.random() * 6) + 1;
-		System.out.println("DieValue: " + die_value);
-	}
+	// Variables and constant initialized
+	public static int POSITION_OF_PLAYER = 0;
+	public static int RANDOM_DIE_VALUE = (int) (Math.random() * 6) + 1;
+	public static int RANDOM_CHECK = (int) (Math.random() * 10) % 3;
 
 	public static void main(String[] args) {
-		// Welcome initialize message
-		System.out.println("Welcome to the Snake & Ladder Game");
-		SnakeAndLadderGame obj1 = new SnakeAndLadderGame();
-		obj1.dieRoll();
+		System.out.println("RandomValue: " + RANDOM_CHECK);
+		switch (RANDOM_CHECK) {
+		case 1:
+			System.out.println("Ladder");
+			System.out.println("Dice value: " + RANDOM_DIE_VALUE);
+			POSITION_OF_PLAYER += RANDOM_DIE_VALUE;
+			System.out.println("Player position is: " + POSITION_OF_PLAYER);
+			break;
+			
+		case 2:
+			System.out.println("Snake");
+			System.out.println("Dice value: " + RANDOM_DIE_VALUE);
+			POSITION_OF_PLAYER -= RANDOM_DIE_VALUE;
+			System.out.println("Player position is: " + POSITION_OF_PLAYER);
+			break;
+			
+		case 0:
+			System.out.println("No Play");
+			POSITION_OF_PLAYER = POSITION_OF_PLAYER;
+			System.out.println("Player position is: " + POSITION_OF_PLAYER);
+			break;
+
+		default:
+			System.out.println("Something Unexpectated Happen !!");
+
+		}
 	}
+
 }
